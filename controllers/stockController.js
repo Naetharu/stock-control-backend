3802eea82d50a000731e4c574354f8ed12d9ceca
James Bridge <naetharu@gmail.com>
2021-09-06 09:55:45 +0100

    added methods to stockController

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,9 +1,23 @@
+// Create
+exports.create_stock_item = (req, res) => {
+  res.send("Creating a stock item");
+};
+
+// Read
 exports.get_stock_all = (req, res) => {
-  res.send(
-    "Welcome to the stock router get function, via the stock controller"
-  );
+  res.send("finding all stock items");
 };
 
 exports.get_stock_one = (req, res) => {
-  res.send("Welcome to the stock router get one function");
+  res.send("finding one stock item");
+};
+
+// Update
+exports.update_stock_item = (req, res) => {
+  res.send("updating stock item");
+};
+
+// Delete
+exports.delete_stock_item = (req, res) => {
+  res.send("deleteing stock item");
 };
---
+++ models/stockItem.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const StockItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Ordered", "Stock", "Build", "Ready", "Deployed", "Scrapped"],
+  },
+  contract: {
+    type: String,
+    required: false,
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+  serial: {
+    type: String,
+    required: false,
+  },
+  asset: {
+    type: String,
+    required: false,
+  },
+});
+
+StockItemSchema.virtua("url").get(() => {
+  return "/stock/" + this._id;
+});
+
+module.exports = mongoose.model("StockItem", StockItemSchema);
